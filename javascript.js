eba151baff074576af9e3bbd968e02df5ff228fb
adnanmulalic <adnan.mulalic3@gmail.com>
2022-11-12 13:01:43 +0100

    wip

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
+const board = document.querySelector("#board");
+const boardTiles = document.querySelectorAll(".boardTile");
+
 const gameBoard = (() => {
-    const gameBoardArray = ["x", "o"];
+    const gameBoardArray = ["2", "8"];
+    const displayMoves = () => {
+        for (let i = 0; i < gameBoardArray.length; i++) {
+            for (let j = 0; j < boardTiles.length; j++) {
+                if (gameBoardArray[i] === boardTiles[j].id) {
+                    boardTiles[j].innerHTML = "X";
+                    console.log("Found a match");
+                }
+            }
+        }
+    }
     return {
         gameBoardArray,
+        displayMoves,
     };
 })();
+
+
+
 const Players = (name) => {
 
 }
